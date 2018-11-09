03d7dcfbe8cd164dfd4d80406929dc1fd9401a7c
neavemj <matthewjneave1@gmail.com>
2018-11-09 17:11:23 +1100

    added rearranging option

--- rearrange_contigs_ref.py
+++ rearrange_contigs_ref.py
@@ -16,7 +16,7 @@ import glob
 
 parser = argparse.ArgumentParser("take viral genome(s) and calculate coordinates for the contigs along a given "
                                  "reference genome"
-                                 "region\nnote: requires mummer to be available on the command line\nnote: "
+                                 "\nnote: requires mummer to be available on the command line\nnote: "
                                  "requires that biopython is installed in python\n")
 
 parser.add_argument('-g', '--genomes', type = str,
@@ -44,15 +44,26 @@ if args.genomes is None or args.output is None or args.reference_genome is None:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
-# check that blast is loaded
+# check that dependancies are loaded
 
 try:
     tmp = subprocess.call(["nucmer", "-version"])
-except OSError as e:
-    if e.errno == os.errno.ENOENT:
-        print("*** error: nucmer could not be found: try 'module load mummer'\n")
-        raise
+except:
+    print("*** error: nucmer could not be found: trying to 'module load mummer'\n")
+    subprocess.call(["module", "load", "mummer"])
 
+try:
+    tmp = subprocess.call(["R", "--version"])
+except:
+    subprocess.call(["module", "load", "R"])
+
+# function to make a dictionary of contigs from a multi-fasta genome
+
+def make_genome_dict(gen):
+    genome_dict = {}
+    for contig in SeqIO.parse(gen, "fasta"):
+        genome_dict[contig.id] = contig.seq
+    return(genome_dict)
 
 # align contigs to the reference using mummer and nucmer (nucleotide alignment)
 # if the genomes are more divergent they can be aligned using promer (protein alignment)
@@ -69,6 +80,29 @@ for genome in args.genomes:
     tiling_output = subprocess.check_output(["show-tiling", "-c", "-g", "-1", genome + ".delta"])
     tiling_output = tiling_output.decode("utf-8")
 
+    # need to re-run mummer but without the circularisation flag (-g)
+    # this will make it easier to re-arrange them appropriately
+    if args.arranged_contigs:
+        rearrange_output = subprocess.check_output(["show-tiling", "-g", "-1", genome + ".delta"])
+        rearrange_output = rearrange_output.decode("utf-8")
+        print(rearrange_output)
+        genome_dict = make_genome_dict(genome)
+        new_genome = open(args.arranged_contigs[0], "w")
+        genome_name = genome.split(".")[0]
+        new_genome.write(">" + genome_name + "_contigs_arranged" + "\n")
+        for contig in tiling_output.split("\n"):
+            contig = contig.strip().split("\t")
+            if len(contig) < 7:
+                continue
+            contig_name = contig[7]
+            strand = contig[6]
+            if strand == "+":
+                new_genome.write(str(genome_dict[contig_name]))
+            if strand == "-":
+                rev_comp = genome_dict[contig_name].reverse_complement()
+                new_genome.write(str(rev_comp))
+
+
     # b'>AF369029.2 292967 bases\n-195054\t74406\t2360\t269461\t98.19\t99.63\t+\tscaffold_0\n76767\t83037\t14875\t6271
     # \t100.00\t99.86\t+\tscaffold_1\n'
 
@@ -78,19 +112,21 @@ for genome in args.genomes:
             continue
         contig_name = contig[7]
         genome_name = genome.split(".")[0]
-
         start = int(contig[0])
+        end = int(contig[1])
+        length = int(contig[3])
         wrapped = "False"
         if start < 0:
-            # first write contig from back-half of the wrap
+            # write contig from first-half of the wrap
+            wrapped = "True"
+            out_handle.write("\t".join([genome_name, contig_name, "0", contig[1], contig[3], contig[4],
+                                        contig[5], contig[6], wrapped]) + "\n")
+
+            # write contig from back-half of the wrap
             ref_record = SeqIO.read(args.reference_genome[0], "fasta")
             ref_len = len(ref_record.seq)
             start = ref_len -- start
-            wrapped = "True"
             out_handle.write("\t".join([genome_name, contig_name, str(start), str(ref_len), contig[3], contig[4],
-                                        contig[5], contig[6], wrapped]) + "\n")
-            # now write contig from first-half of the wrap
-            out_handle.write("\t".join([genome_name, contig_name, "0", contig[1], contig[3], contig[4],
                                         contig[5], contig[6], wrapped]) + "\n")
 
         else:
@@ -104,18 +140,4 @@ out_handle.close()
 
 if args.plot:
     subprocess.call(["/datastore/nea040/PycharmProjects/genome_tools/plot_mummer.R", args.output[0]])
-
-
-# create genome record and reverse complement if required
-
-#if strand == "minus":
-#    print("~~~ reverse complementing genome")
-#    genome_record = genome_record.reverse_complement(id=True, name=True, description=True)
-#    # also need to change the new start position
-#    start = len(genome_record.seq) - (int(start) - 1) # python 0-based indexing fix
-
-#if float(identity) < 80:
-#    print("*** warning: identity is low for the VP10 gene")
-#    # TODO: maybe stop re-arrangement if identity is too low
-
 
